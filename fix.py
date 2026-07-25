c2ff8d809175dd2812754daa4acc55a1cebadc34
cppservergit <cppserver@martincordova.com>
2026-07-25 08:32:39 -0400

    Remove temporary refactoring python scripts from repository

--- fix.py
+++
@@ -1,8 +0,0 @@
-import re
-with open("src/handlers.c", "r") as f:
-    s = f.read()
-s = s.replace("[[maybe_unused]] \n    struct evbuffer", "struct evbuffer")
-s = s.replace("[[maybe_unused]] \n", "")
-s = s.replace("int* out_status, \n    \n    struct evbuffer", "int* out_status, \n    struct evbuffer")
-with open("src/handlers.c", "w") as f:
-    f.write(s)
