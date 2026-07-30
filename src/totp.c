343fe08b5e6f71383cf1dc74a6065d30cb5e60f9
cppservergit <cppserver@martincordova.com>
2026-07-30 12:53:38 -0400

    Replace custom totp_urlencode with evhttp_uriencode

--- src/totp.c
+++ src/totp.c
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <liboath/oath.h>
 #include <time.h>
+#include <event2/http.h>
 
 static int get_secret(const char* user, char* out_secret, size_t max_len) {
     SQLHDBC hdbc = odbcutil_connect(DB_0);
@@ -62,23 +63,7 @@ static void totp_append_qr_svg_path(struct evbuffer* out_buf, QRcode *qrcode) {
     }
 }
 
-static void totp_urlencode(const char* src, char* dest, size_t dest_size) {
-    if (!src || !dest || dest_size == 0) return;
-    size_t j = 0;
-    const char hex[] = "0123456789ABCDEF";
-    for (size_t i = 0; src[i] && j < dest_size - 3; ++i) {
-        unsigned char c = (unsigned char)src[i];
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || 
-            (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.' || c == '~') {
-            dest[j++] = (char)c;
-        } else {
-            dest[j++] = '%';
-            dest[j++] = hex[c >> 4];
-            dest[j++] = hex[c & 0x0F];
-        }
-    }
-    dest[j] = '\0';
-}
+
 
 void totp_generate_svg(const char* user, int* out_status, struct evbuffer* out_buf) {
     if (!user) {
@@ -94,8 +79,12 @@ void totp_generate_svg(const char* user, int* out_status, struct evbuffer* out_b
         return;
     }
 
-    char escaped_user[256];
-    totp_urlencode(user, escaped_user, sizeof(escaped_user));
+    char* escaped_user = evhttp_uriencode(user, -1, 0);
+    if (!escaped_user) {
+        *out_status = HTTP_INTERNAL;
+        sodium_memzero(secret, sizeof(secret));
+        return;
+    }
 
     char uri[512];
     (void)snprintf(uri, sizeof(uri), "otpauth://totp/apiserver:%s?secret=%s&issuer=apiserver&algorithm=SHA256&digits=6&period=30", escaped_user, secret);
@@ -103,7 +92,8 @@ void totp_generate_svg(const char* user, int* out_status, struct evbuffer* out_b
     QRcode *qrcode = QRcode_encodeString(uri, 0, QR_ECLEVEL_L, QR_MODE_8, 1);
     
     sodium_memzero(secret, sizeof(secret));
-    sodium_memzero(escaped_user, sizeof(escaped_user));
+    sodium_memzero(escaped_user, strlen(escaped_user));
+    free(escaped_user);
     sodium_memzero(uri, sizeof(uri));
 
     if (!qrcode) {
